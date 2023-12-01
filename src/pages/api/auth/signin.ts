02975c680fea26f11c0e8f6459bc2d2dd39f681d
code-PA-32 <vernichenko.aleksandr@gmail.com>
2023-12-01 13:22:48 +0200

    add Github 0Auth

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,30 +1,26 @@
-import type { APIRoute } from "astro";
-import { supabase } from "@/lib/supabase.ts";
+import type {APIRoute} from "astro";
+import {supabase} from "@/lib/supabase.ts";
 
-export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+export const POST: APIRoute = async ({request, redirect}) => {
   const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
+  const provider = formData.get("provider")?.toString();
 
-  if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
+  const validProviders = ["github"];
+
+  if (provider && validProviders.includes(provider)) {
+    const {data, error} = await supabase.auth.signInWithOAuth({
+      provider: provider as any,
+      options: {
+        redirectTo: "http://localhost:4321/api/auth/callback"
+      },
+    });
+
+    if (error) {
+      return new Response(error.message, {status: 500});
+    }
+
+    return redirect(data.url);
   }
 
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-
-  if (error) {
-    return new Response(error.message, { status: 500 });
-  }
-
-  const { access_token, refresh_token } = data.session;
-  cookies.set("sb-access-token", access_token, {
-    path: "/",
-  });
-  cookies.set("sb-refresh-token", refresh_token, {
-    path: "/",
-  });
-  return redirect("/dashboard");
+  return redirect("/singin");
 };
